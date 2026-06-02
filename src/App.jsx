5334afd0220df46cbb418f977294d6bd921c50a5
Brintik <brintikmajumder@gmail.com>
2026-06-02 20:33:05 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { FiX, FiSend, FiMessageCircle } from 'react-icons/fi';
 import PortfolioHub from './PortfolioHub';
 import CaseStudy from './CaseStudy';
+import posthog from 'posthog-js'
+
+posthog.init('<YOUR_PROJECT_API_KEY>', { 
+  api_host: 'https://app.posthog.com' 
+})
 
 /* Configuration Dictionaries */
 const THEME_CONFIG = {
